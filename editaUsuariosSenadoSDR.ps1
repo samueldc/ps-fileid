5828029ae8ba7ef2eca4685a6cae692088555aee
Samuel Diniz Casimiro <P_7029@redecamara.camara.gov.br>
2020-09-25 19:14:03 -0300

    Novo script para edicao de usuario do senado SDR

---
+++ editaUsuariosSenadoSDR.ps1
@@ -0,0 +1,128 @@
+﻿<#
+Programa: editaUsuariosSenadoSDR
+Objetivo: Edição de usuários para uso no SDR
+Autor: Samuel Diniz Casimiro - P_7029
+Setor: Ditec/Coaus/Satus
+Versões: 1.0 - Criação do script
+#>
+
+# Variáveis globais -----------------------------------------------------------------------
+
+# Se true, ativa o mode de teste (dry-run) nos comandos que utilizam este parâmetro
+$WhatIf = $false
+$Confirm = $false
+
+# Caminho e nome dos arquivos de log
+$PathLog = "C:\Users\p_7029\ownCloud\_trabalho\teletrabalho\sdr\editaUsuariosSenadoSDR-" + (Get-Date -UFormat %Y-%m-%d-%H-%M) + ".log"
+$PathLogErro = "C:\Users\p_7029\ownCloud\_trabalho\teletrabalho\sdr\editaUsuariosSenadoSDR-" + (Get-Date -UFormat %Y-%m-%d-%H-%M) + ".logerro"
+
+# Caminho e nome do arquivo CSV com a lista de usuarios a ser importada
+$PathLista = "C:\Users\p_7029\ownCloud\_trabalho\teletrabalho\sdr\usuariosSenadoSDR.csv"
+
+<#
+Os contadores a seguir servem para gerar um resumo das ações ao final da execução do script.
+Esse resumo é incluído no final do arquivo de log gerado.
+#>
+
+# Contadores de usuários
+$UsuariosEditados = 0
+$UsuariosNaoEditados = 0
+$UsuariosTotal = 0
+
+# Aqui começam as ações do script
+
+# Solicita credencial para executar o script (não precisa pq por enquanto estamos utilizando as credenciais do usuário logado na máquina)
+##$Credential = Get-Credential
+
+# Cria arquivo de log e já registra a data e a hora do inicio da execução do script.
+"Início do script: " + (Get-Date) *> $PathLog
+
+# Cria arquivos
+"" *> $PathLogErro
+"nome_sdr;ponto_sdr;senha_sdr" *> $PathListaFinal
+
+# Registra no log a credencial usada para execução do script
+"O script foi executado por: $env:COMPUTERNAME\$env:UserName" *>> $PathLog
+
+# Importar arquivo de CSV
+$ArqLista = $null
+$ArqLista = Import-Csv -Path $PathLista -Delimiter ";" -Encoding Default
+# Verifica se a importacao foi bem sucedida
+if (!$?) { # Se não foi...
+    # Loga o erro
+    "[Import-Csv] Ocorreu um erro ao abrir o arquivo: " + $PathLista *>> $PahLogErro    
+    # Finaliza a sessão com o Exchange
+    Remove-PSSession $Session
+    "Fim do script:" + (Get-Date) *>> $PathLog
+    # Encerra a execução do script
+    Pause
+    Exit
+}
+
+# Se chegou até aqui é pq a importação do arquivo csv foi bem sucedida
+
+# Passa os registros da lista para estrutura de repetição
+$ArqLista | ForEach-Object {
+
+    # Povoa as variáveis
+    
+    # Variáveis gerais
+
+    # Redefine para não correr o risco de viciar os valores (trata-se de uma característica do PowerShell que em caso de erro em comando o valor anterior da variável é mantido)
+    $pontosdr = ""
+    $senhasdr = ""
+    $nomesdr = ""
+
+    # Povoa
+    $pontosdr = $PSItem.ponto_sdr.Trim()
+    $senhasdr = $PSItem.senha_sdr.Trim()
+    $nomesdr = $PSItem.nome_sdr.Trim()
+
+    $usuario = $null
+    
+    $pontosdr
+    # Verifica se o usuario do deputado existe
+    $usuario = Get-ADUser -Identity $pontosdr -Properties *
+
+    # Se o usuário existe, edita o usuário
+    if ($?) {
+
+        # Cria a conta
+        #$usuario = Set-ADUser -Identity $usuario.DistinguishedName -AccountPassword (ConvertTo-SecureString -AsPlainText $senhasdr -Force) -PassThru:$true -Confirm:$Confirm -WhatIf:$WhatIf
+        Set-ADAccountPassword -Identity $usuario.DistinguishedName -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $senhasdr -Force) -Confirm:$Confirm -WhatIf:$WhatIf
+            if($?){"[Set-ADUser] Usuario $pontosdr editado." *>> $PathLog}
+                else{"[Set-ADUser] Ocorreu um erro ao editar o usuario $pontosdr." *>> $PahLogErro}
+
+            # Adiciona o usuário nos grupos padrão
+            #Add-ADGroupMember -Identity InfolegSimulacaoSDR -Members $usuario.DistinguishedName -Confirm:$Confirm -WhatIf:$WhatIf
+            #Add-ADGroupMember -Identity NegarAcessoWifi -Members $usuario.DistinguishedName -Confirm:$Confirm -WhatIf:$WhatIf
+            #Add-ADGroupMember -Identity Negar_Logon_local_RDP -Members $usuario.DistinguishedName -Confirm:$Confirm -WhatIf:$WhatIf
+                #if($?){"[Add-ADGroupMember] Usuário $pontosdr com permissoes incluidas." *>> $PathLog}
+                    #else{"[Add-ADGroupMember] Ocorreu um erro ao incluir permissoes para o usuário $pontosdr." *>> $PahLogErro}
+            $UsuariosEditados++
+    } else {
+
+        "[New-Mailbox] Usuário $pontosdr não existe." *>> $PahLogErro
+        $UsuariosNaoEditadoss++
+
+    }
+
+    $UsuariosTotal++
+
+}
+
+# Registra no Log um relatório da execução do Script
+"Relatório de usuários:" *>> $PathLog
+"Total de usuários na lista: $UsuariosTotal" *>> $PathLog
+"Usuários criados: $UsuariosEditados" *>> $PathLog
+"Usuários com erro na criação: $UsuariosNaoEditados" *>> $PathLog
+
+if ($UsuariosTotal -ne ($UsuariosEditados + $UsuariosNaoEditados)) {
+    "ATENÇÃO: Existe uma diferença entre a qtde. de usuários total e a qtde. de usuários editados mais os não editados." *>> $PathLog
+    ">>>>>>>> Sugere-se revisar os dados do arquivo csv." *>> $PathLog
+}
+
+# Registra no Log a data e a hora do FIM da execução do script.
+"Fim do script: $(Get-Date)" *>> $PathLog
+
+# Fim do Script ------------------------------------------------------------------
